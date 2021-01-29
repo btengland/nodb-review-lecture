1167e20d0a5b91f3c40251281624e7c04444897b
Brady England <bradyeng22@gmail.com>
2021-01-29 11:31:13 -0700

    Done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import './App.css';
+import './reset.css'
+import './App.css'
+import Header from './components/Header'
+import Main from './components/Main'
 
 function App() {
   return (
     <div >
-      
+        <Header/>
+        <Main/>
     </div>
   );
 }
---
+++ src/components/Form.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react'
+
+class Form extends Component {
+    constructor(){
+        super()
+        this.state = {
+            task:''
+        }
+    }
+
+    handleChange = e => {
+        this.setState({
+            task: e.target.value
+        })
+    }
+
+    handleSubmit = e => {
+        e.preventDefault()
+        this.props.addTodo(this.state.task)
+        this.setState({
+            task:''
+        })
+    }
+
+    render(){
+        return <div className='form-container'>
+            <form onSubmit={(e) => this.handleSubmit(e)}>
+                <input onChange={e => this.handleChange(e)} 
+                value={this.state.task} 
+                placeholder='Type Here' 
+                type='text'/>
+                <button type='submit'>Add Todo</button>
+            </form>
+        </div>
+    }
+}
+
+export default Form
